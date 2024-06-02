247f4ae4ddc951ee94d9416b73998944746a42ea
Jaycom17 <juanorejuela499@gmail.com>
2024-06-02 12:21:31 -0500

    products was fixed

--- src/middlewares/ProductMiddleware.py
+++ src/middlewares/ProductMiddleware.py
@@ -2,9 +2,8 @@ from models.ProductModel import ProductModel
 
 def productMiddleWare(product):
     try:
-        return ProductModel(product["proName"], product["proStock"], product["proHeight"], product["proWidth"], product["proLength"], product["proWeight"], product["proBuyPrice"], product["proSellPrice"], product["proMinStock"], product["proMaxStock"], product["proDescription"], product["proTypeID"])
+        return ProductModel(product["proName"], product["proStock"], product["proHeight"], product["proLength"], product["proWidth"], product["proWeight"], product["proBuyPrice"], product["proSellPrice"], product["proMinStock"], product["proMaxStock"], product["proDescription"], product["proTypeID"])
     except KeyError as e:
         return None
-
 
 
