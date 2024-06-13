8084169267c293589e823712669eed05c602045d
JoseENarvaezM <josenarvaezm2003@gmail.com>
2024-06-13 08:54:02 -0500

    product swagger

--- src/index.py
+++ src/index.py
@@ -9,6 +9,9 @@ from routes.DealersRoutes import setupRoutesDealer
 from routes.ShoppingsRoutes import setupRoutesShopping
 from routes.ProductsRoutes import setupRoutesProduct
 
+#swagger
+from flask_swagger_ui import get_swaggerui_blueprint
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.join(os.getcwd(),'images')
@@ -18,6 +21,28 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 def init_app(config):
+    SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+    API_URL = '/static/openapi.json'  # Our API url (can of course be a local resource)
+
+    # Call factory function to create our blueprint
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+        API_URL,
+        config={  # Swagger UI config overrides
+            'app_name': "Test application"
+        },
+        # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+        #    'clientId': "your-client-id",
+        #    'clientSecret': "your-client-secret-if-required",
+        #    'realm': "your-realms",
+        #    'appName': "your-app-name",
+        #    'scopeSeparator': " ",
+        #    'additionalQueryStringParams': {'test': "hello"}
+        # }
+    )
+
+    app.register_blueprint(swaggerui_blueprint)
+    
     # Configuration
     app.config.from_object(config)
 
